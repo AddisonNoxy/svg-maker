ec9110ce7575a178d3ed1518e905c428aec553f1
AddisonNoxy <drolknil@gmail.com>
2023-07-21 23:48:35 -0400

    finalized tests, wrote inquirer code

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
+const {Square, Triangle, Circle} = require("./lib/shapes.js");
 
 inquirer.prompt([
     {
@@ -9,25 +11,76 @@ inquirer.prompt([
     {
         type: 'input',
         message: 'What color will the text be in hexadecimals?',
-        name: 'text-color'
+        name: 'textcolor'
     },
     {
         type: 'list',
         message: 'What shape will it be?',
-        name: 'shape'
+        name: 'shape',
+        choices: ['triangle', 'square', 'circle']
     },
     {
         type: 'input',
         message: 'What color is the shape in hexadecimals?',
-        name: 'shape-color'
+        name: 'shapecolor'
     }
 ])
 .then((response) => 
     promptsValid(response) == true
-        ? console.log("SVG file created!")
+        ? createFile(response.shape, response.letters, response.textcolor, response.shapecolor)
         : console.log("Please provide valid responses."));
 
 function promptsValid(inputs) {
     var checks = 0;
+
+    if (inputs.letters.length <= 3) {
+        checks++;
+    } else {
+        return false;
+    }
+
+    let letters = /^[A-Za-z]+$/;
+
+    if (inputs.textcolor.match(letters)) {
+        checks++
+    } else {
+        return false;
+    }
+
+    if (inputs.shapecolor.match(letters)) {
+        checks++
+    } else {
+        return false;
+    }
+
+    return true;
     
+}
+
+function createFile(shape, text, textColor, shapeColor) {
+    switch (shape) {
+        case "triangle":
+            var newShape = new Triangle(300, 300, "white");
+            break;
+        case "square":
+            var newShape = new Square(300, 300, "white");
+            break;
+        case "circle":
+            var newShape = new Circle(300, 300, "white");
+            break;
+        default:
+            var newShape = new Triangle(300, 300, "white");
+    }
+
+    newShape.setText(text);
+    newShape.setColor(shapeColor);
+    newShape.setTextColor(textColor);
+
+    console.log(newShape.fullRender());
+
+    fs.writeFile('./examples/example.svg', newShape.fullRender(), function (e) {
+        console.log("Error!");
+    });
+
+    console.log("SVG file created!")
 }
